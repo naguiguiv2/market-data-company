c62d653274e8595f638b94ea8885943c0a595fa5
Andrew Nguyen <andrewvonguyen24@hotmail.com>
2019-06-09 20:44:44 -0400

    mend

--- components/animated-footer-link/AnimatedFooterLink.styled.js
+++ components/animated-footer-link/AnimatedFooterLink.styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const AnimatedFooterLinkWrapper = styled.div`
   display: flex;
   flex-direction: row;
-  align-items: center;
+  align-items: baseline;
   flex: 1;
 `;
 
