a339ee851a24dd788d84d02d00f917f398fbffcc
Andrew Nguyen <andrewvonguyen24@hotmail.com>
2019-08-26 19:24:57 -0400

    MDC-25: Carousel images (#18)
    * MDC-25: Add carousel images
    
    * MDC-25: Fixed animated card height

--- sections/home-section/worked-with/index.js
+++ sections/home-section/worked-with/index.js
@@ -9,25 +9,29 @@ const COMPANY_DATA = [
 		title: 'Canada Pension Plan Investment Board',
 		description: `“MDC identified and realized vendor cost savings of 12% of our spend, and then developed a target state organization with capabilities to effectively manage market data products, consumption, costs and vendors”`,
 		role: 'Marc Odho – Head of Data Management',
-		company: 'PSP'
+		company: 'PSP',
+		imageUrl: 'cppib-carousel'
 	},
 	{
 		title: 'PSP Investissements',
 		description: `“The team at Market Data Company is great to work with. Each consultant has their own strengths and weaknesses, and are great at what they do. In my book, I highly recommend the entire team at MDC.”`,
 		role: 'Alex DiCristofano, Director of Market Data',
-		company: 'Aviva'
+		company: 'Aviva',
+		imageUrl: 'psp-carousel'
 	},
 	{
 		title: 'Aviva Investors',
 		description: `“Robert and his team brought the right expertise to the job, making the transitional program easier to manage. MDC’s approach to market data supported our Global operations and provided a clear line of sight to our vendor spend.”`,
 		role: 'Kelly McManis, Head of Operations',
-		company: 'Swiss Re'
+		company: 'Swiss Re',
+		imageUrl: 'aviva-carousel'
 	},
 	{
 		title: 'Swiss Re',
 		description: `“MDC’s product Kiaro’s overspend report clearly articulated where Bloomberg data was being replicated within our technology envirorment. As a result we closed down several jobs that lead to $450,000 in cost savings.”`,
 		role: 'Eric Kemeny, Vice President, Group Asset Management',
-		company: 'CPPIB'
+		company: 'CPPIB',
+		imageUrl: 'swiss-re-carousel'
 	}
 ]
 
@@ -64,6 +68,7 @@ const WorkedWith = () => {
 					description={item.description}
 					role={item.role}
 					company={item.company}
+					imageUrl={item.imageUrl}
 					onNext={next}
 				/>
 			</CarouselItem>
