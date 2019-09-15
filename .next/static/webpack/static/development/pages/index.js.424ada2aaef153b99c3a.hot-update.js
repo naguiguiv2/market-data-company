28aabc962e35d9a24378fa18fc71dbdd679985f6
Justin White <justin.gj.white@gmail.com>
2019-09-15 12:15:30 -0400

    Cache changes
    A bunch of cache changes that just seem to pile up. I didn't change anything

---
+++ .next/static/webpack/static/development/pages/index.js.424ada2aaef153b99c3a.hot-update.js
@@ -0,0 +1,106 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./sections/home-section/landing/Landing.styled.js":
+/*!*********************************************************!*\
+  !*** ./sections/home-section/landing/Landing.styled.js ***!
+  \*********************************************************/
+/*! exports provided: LandingWrapper, ContentWrapper, Title, Description, ButtonWrapper */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "LandingWrapper", function() { return LandingWrapper; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ContentWrapper", function() { return ContentWrapper; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Title", function() { return Title; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Description", function() { return Description; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ButtonWrapper", function() { return ButtonWrapper; });
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/taggedTemplateLiteral */ "./node_modules/@babel/runtime-corejs2/helpers/esm/taggedTemplateLiteral.js");
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+/* harmony import */ var styled_components_breakpoint__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! styled-components-breakpoint */ "./node_modules/styled-components-breakpoint/dist/esm/index.js");
+
+
+function _templateObject5() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__["default"])(["\n    max-width: 340px;\n  "]);
+
+  _templateObject5 = function _templateObject5() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject4() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__["default"])(["\n    font-size: 24px;\n    line-height: 32px;\n  "]);
+
+  _templateObject4 = function _templateObject4() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject3() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__["default"])(["\n    font-size: 57px;\n    line-height: 64px;\n  "]);
+
+  _templateObject3 = function _templateObject3() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject2() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__["default"])(["\n\t\tbackground-image: url(\"/static/images/desktop-landing.png\");\n  "]);
+
+  _templateObject2 = function _templateObject2() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__["default"])(["\n\t\tbackground-image: url(\"/static/images/tablet-landing.jpg\");\n\t"]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+
+
+var LandingWrapper = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].div.withConfig({
+  displayName: "Landingstyled__LandingWrapper",
+  componentId: "t8svsq-0"
+})(["background-image:url('/static/images/mobile-landing.jpg');background-size:cover;width:100%;", " ", ""], Object(styled_components_breakpoint__WEBPACK_IMPORTED_MODULE_2__["default"])('tablet')(_templateObject()), Object(styled_components_breakpoint__WEBPACK_IMPORTED_MODULE_2__["default"])('desktop')(_templateObject2()));
+var ContentWrapper = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].div.withConfig({
+  displayName: "Landingstyled__ContentWrapper",
+  componentId: "t8svsq-1"
+})(["max-width:1200px;margin:0 auto;padding:0 20px;display:flex;flex-direction:column;flex:1;"]);
+var Title = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].h1.withConfig({
+  displayName: "Landingstyled__Title",
+  componentId: "t8svsq-2"
+})(["font-family:", ";font-size:42px;line-height:44px;color:", ";padding:40px 0 12px 0;margin-bottom:16px;", ""], function (props) {
+  return props.theme.fonts.graphikBold;
+}, function (props) {
+  return props.theme.colors.white;
+}, Object(styled_components_breakpoint__WEBPACK_IMPORTED_MODULE_2__["default"])('desktop')(_templateObject3()));
+var Description = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].h1.withConfig({
+  displayName: "Landingstyled__Description",
+  componentId: "t8svsq-3"
+})(["font-family:", ";font-weight:400;font-size:21px;line-height:24px;color:", ";margin:0 0 42px 0;text-align:left;max-width:720px;", ""], function (props) {
+  return props.theme.fonts.financier;
+}, function (props) {
+  return props.theme.colors.white;
+}, Object(styled_components_breakpoint__WEBPACK_IMPORTED_MODULE_2__["default"])('desktop')(_templateObject4()));
+var ButtonWrapper = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].div.withConfig({
+  displayName: "Landingstyled__ButtonWrapper",
+  componentId: "t8svsq-4"
+})(["display:flex;flex-direction:row;justify-content:space-between;max-width:360px;margin:0 0 40px 0;", ""], Object(styled_components_breakpoint__WEBPACK_IMPORTED_MODULE_2__["default"])('desktop')(_templateObject5()));
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.424ada2aaef153b99c3a.hot-update.js.map
