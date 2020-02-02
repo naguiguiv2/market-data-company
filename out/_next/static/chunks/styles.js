554c702c1843ad1fdb9ced02741e8dc7f1892c38
Justin White <justin.gj.white@gmail.com>
2020-02-02 18:41:19 -0500

    Cache changes

--- out/_next/static/chunks/styles.js
+++ out/_next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1569888191213");
+            injectCss(link, link.href.split("?")[0] + "?unix=1570397284680");
           });
       }
     }
