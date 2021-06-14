aa6008999e2f9484ffff90321e2e9ceeb5ca3241
wyao905 <warrenyca905@gmail.com>
2021-06-13 20:43:22 -0400

    refetching everything

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/research":["static\u002Fchunks\u002Fpages\u002Fresearch.js"],"/solutions":["static\u002Fchunks\u002Fpages\u002Fsolutions.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fresearch","\u002Fsolutions"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ components/solutions-dropdown-menu/index.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+// import * as S from './SolutionsDropdownMenu.styled'
+
+const SolutionsDropdownMenu = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default SolutionsDropdownMenu
